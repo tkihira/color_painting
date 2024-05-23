ac793453bfe6bb29e1a6150fcb5c1abc2e05ba96
tkihira <takuo.kihira@gmail.com>
2024-05-23 12:58:24 +0900

    generate images from serverless

--- api/generate.js
+++ api/generate.js
@@ -1,7 +1,40 @@
-const generate = async () => {
-    const b64string = "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAIAAADTED8xAAADMElEQVR4nOzVwQnAIBQFQYXff81RUkQCOyDj1YOPnbXWPmeTRef+/3O/OyBjzh3CD95BfqICMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMO0TAAD//2Anhf4QtqobAAAAAElFTkSuQmCC";
-    const buffer = Buffer.from(b64string, "base64");
-    return buffer;
+const engineId = 'stable-diffusion-v1-6';
+const apiHost = process.env.API_HOST ?? 'https://api.stability.ai';
+const apiKey = process.env.STABILITY_API_KEY;
+
+const generate = async (text) => {
+    const response = await fetch(
+        `${apiHost}/v1/generation/${engineId}/text-to-image`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body: JSON.stringify({
+                text_prompts: [
+                    {
+                        text: text + ', white and black, coloring book for kids, simple, adult coloring book, no detail, outline no color, fill frame, edge to edge, clipart white background',
+                    },
+                ],
+                cfg_scale: 7,
+                height: 1024,
+                width: 1024,
+                steps: 30,
+                samples: 1,
+            }),
+        }
+    );
+
+    if (!response.ok) {
+        throw `${response.status} response: ${await response.text()}`;
+    }
+
+    const responseJSON = await response.json();
+
+    const image = responseJSON.artifacts[0];
+    return Buffer.from(image.base64, 'base64');
 };
 
 export default async (req, res) => {
@@ -10,16 +43,16 @@ export default async (req, res) => {
             const text = JSON.parse(req.body).text?.trim();
             if (text) {
                 try {
-                    const buffer = await generate();
+                    const buffer = await generate(text);
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'image/png');
                     res.end(buffer);
-                } catch(e) {
-                    if(typeof e === "string") {
-                        res.status(500).json({error: e});
+                } catch (e) {
+                    if (typeof e === "string") {
+                        res.status(500).json({ error: e });
                     } else {
                         console.error(e);
-                        res.status(500).json({error: 'Internal Error'});
+                        res.status(500).json({ error: 'Internal Error' });
                     }
                 }
                 // res.status(200).json({ echoedText: text });
